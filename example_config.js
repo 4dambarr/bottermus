9d5ee5d85b1d93d782bb483c78f948da6d4c005e
4dambarr <ab2474@york.ac.uk>
2021-03-08 20:36:22 +0000

    Initial Commit

---
+++ example_config.js
@@ -0,0 +1,8 @@
+let config = {
+    consumer_key: "consumer key",
+    consumer_secret: "consumer secret key",
+    access_token: "access toke",
+    access_token_secret: "secret access token"
+};
+
+module.exports = config;
