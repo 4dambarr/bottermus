7d7a86b4bc0e6bf49c45110fab3a2d7c0c93e7fb
4dambarr <ab2474@york.ac.uk>
2021-03-11 00:01:43 +0000

    Retweet All Stoke Tweets

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+  
 var Twit = require('twit');
 const fs = require('fs')
 
@@ -29,64 +30,13 @@ var T = new Twit({
     access_token_secret: config.access_token_secret,
 });
 
-const helloOne = fs.readFileSync('media/hello_1.gif', { encoding: 'base64' })
-const helloTwo = fs.readFileSync('media/hello_2.gif', { encoding: 'base64' })
+const stokeID = '44335177';
+//const stokeID = '1353088861115740161';
+// ON MENTION
+const onMention = require('./StreamFunctions/Mention');
+const onStokeTweet = require('./StreamFunctions/StokeAccountTweet');
 
-function onTweet(tweet) {
-    console.log('tweet recieved:', tweet.text)
-
-    var randomNum = Math.floor(Math.random() * 2)
-    var responseGif;
-    switch (randomNum) {
-        case 0:
-            responseGif = helloOne;
-            break
-        case 1:
-            responseGif = helloTwo;
-            break
-        default:
-            responseGif = helloOne;
-    }
-    console.log('uploading gif')
-    T.post('media/upload', { media_data: responseGif }, function (err, data, response) {
-        if (err) {
-            console.log(err)
-        }
-        var mediaID = data.media_id_string
-        var altText = "Pottermus greeting gif"
-        var meta_params = {
-            media_id: mediaID,
-            alt_text: { text: altText }
-        }
-
-        console.log('Setting gif metadata')
-        T.post('media/metadata/create', meta_params, function (err, data, response) {
-            if (!err) {
-                var reply_params = {
-                    in_reply_to_status_id: tweet.id_str,
-                    status: `Hi ${tweet.user.name}! I am Bottermus, a twitter bot for all things Stoke, I'm currently under development, but heres a gif of me to keep you entertained.`,
-                    auto_populate_reply_metadata: true,
-                    media_ids: [mediaID]
-                }
-
-                console.log(reply_params)
-
-                console.log('sending response')
-                T.post('statuses/update', reply_params, function (err, data, response) {
-                    if (err) {
-                        console.log(err)
-                    } else {
-                        console.log('done')
-                    }
-                })
-            } else {
-                console.log(err)
-            }
-        })
-    })
-}
-
-var stream = T.stream('statuses/filter', { track: '@bottermus' }, (err, reply) => {
+var stream = T.stream('statuses/filter', { track: ['@bottermus'], follow: [stokeID] }, (err, reply) => {
     if (err) {
         console.error(err)
     } else {
@@ -94,7 +44,13 @@ var stream = T.stream('statuses/filter', { track: '@bottermus' }, (err, reply) =
     }
 });
 
-stream.on('tweet', onTweet)
+stream.on('tweet', (tweet) => {
+    if (tweet.in_reply_to_status_id === null && tweet.text.includes('@bottermus')) {
+        onMention(tweet, T)
+    } else if (tweet.in_reply_to_status_id === null && tweet.user.id_str === stokeID){
+        onStokeTweet(tweet, T)
+    }
+})
 
 console.log('Running')
 
