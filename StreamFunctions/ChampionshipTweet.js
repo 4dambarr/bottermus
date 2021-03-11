6033d126c5decfd6a41f85725bb9e3fdab76685e
4dambarr <ab2474@york.ac.uk>
2021-03-11 21:38:40 +0000

    Retweet Championship tweet about Stoke

---
+++ StreamFunctions/ChampionshipTweet.js
@@ -0,0 +1,10 @@
+function onChampionshipTweet(tweet, T) {
+  // Very simple if tweet conatains stoke retweet
+  if (tweet.text.toLowerCase().includes('stoke')) {
+    T.post('/statuses/retweet/:id', { id: tweet.id_str }, function (err, data, response) {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+}
